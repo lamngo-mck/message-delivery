09576f032a692dbba1377305baf3c7aead652996
lamngo-mck <lam_ngo_phan_nhat@mckinsey.com>
2023-06-27 15:06:24 +0700

    feat: handle event collector (draft version)

--- application/port/out/message_repository.go
+++
@@ -1,9 +0,0 @@
-package out
-
-import "message-delivery/application/domain"
-
-type MessageRepository interface {
-	Create(message domain.Message) error
-	GetByID(id string) (domain.Message, error)
-	Delete(id string) error
-}
